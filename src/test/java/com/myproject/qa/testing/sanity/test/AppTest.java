6d92e7c78aefc8f59f4bc7d2e461aac3df87ce41
Raman Ojha <ojha.raman99@gmail.com>
2020-01-18 22:56:51 +0530

    first commit

---
+++ src/test/java/com/myproject/qa/testing/sanity/test/AppTest.java
@@ -0,0 +1,16 @@
+package com.myproject.qa.testing.sanity.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue( true );
+    }
+}
