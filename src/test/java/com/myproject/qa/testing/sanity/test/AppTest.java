3fc7212485764b1b08bafe0481fd8b72c748864c
Raman Ojha <ojha.raman99@gmail.com>
2020-01-23 22:46:23 +0530

    Updated the test class

--- src/test/java/com/myproject/qa/testing/sanity/test/AppTest.java
+++ src/test/java/com/myproject/qa/testing/sanity/test/AppTest.java
@@ -3,10 +3,9 @@ package com.myproject.qa.testing.sanity.test;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+import com.myproject.qa.testing.framework.selenium.BaseWebDriver;
+
+public class AppTest extends BaseWebDriver 
 {
     @Test
     public void shouldAnswerWithTrue()
